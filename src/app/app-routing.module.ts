769559882310bd5c57840764f0f5cb67b41f56ff
mohamessi <mo-2010@live.fr>
2019-05-05 14:25:52 +0200

    modify default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddFlightsComponent } from './add-flights/add-flights.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {
     path:'',
-    component: AddFlightsComponent,
+    component: LoginComponent,
     pathMatch: 'full'
   }
 ];

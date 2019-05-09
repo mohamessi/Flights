db4d77c402ee5ac8e2e80311f054c145fe830b7c
ouin85 <bouinten@yahoo.fr>
2019-05-09 09:39:05 +0200

    Simulate log in with array users and localStorage add log.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AddFlightsComponent } from './add-flights/add-flights.component';
 import { LoginComponent } from './login/login.component';
-
 
 const routes: Routes = [
   {
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,7 @@
 import { Fligth } from './fligth';
 
-export class User {
-    login: string;
-    password: string;
-    role: string;
-    bookings: Array<Fligth>;
+export class User 
+{
+    constructor(public username: string, public password: string, public role: string, 
+                public bookings?: Array<Fligth>) { }
 }
